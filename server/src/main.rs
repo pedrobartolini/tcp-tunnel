573d35b7fe3fc82b7cadfe10a2273ec073d61573
pedrobartolini <pedrobartolini@outlook.com>
2025-10-03 01:59:54 -0300

    refactor: simplify stream binding using tokio's copy_bidirectional

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,5 +1,4 @@
 use tokio::io::AsyncReadExt;
-use tokio::io::AsyncWriteExt;
 use tokio::net::TcpListener;
 use tokio::net::TcpStream;
 use tunnel_common::*;
@@ -49,34 +48,11 @@ async fn validate_tunnel_stream(stream: &mut TcpStream, expected: &str) -> anyho
 async fn bind_streams(mut tunnel_stream: TcpStream, mut local_stream: TcpStream) -> anyhow::Result<()> {
     log::info!("Tunnel and local streams bound");
 
-    let mut tunnel_buffer = [0u8; 4096];
-    let mut local_buffer = [0u8; 4096];
-
-    loop {
-        enum Poll {
-            Tunnel(tokio::io::Result<usize>),
-            Local(tokio::io::Result<usize>)
+    match tokio::io::copy_bidirectional(&mut local_stream, &mut tunnel_stream).await {
+        Ok((from_local, from_tunnel)) => {
+            log::info!("Connection closed. {from_local} bytes sent to tunnel, {from_tunnel} bytes sent to local");
+            Ok(())
         }
-
-        let poll = tokio::select! {
-            biased;
-            res = tunnel_stream.read(&mut tunnel_buffer) => Poll::Tunnel(res),
-            res = local_stream.read(&mut local_buffer) => Poll::Local(res),
-        };
-
-        match poll {
-            Poll::Local(Err(err)) => return Err(anyhow::anyhow!("LocalStream read error: {err}")),
-            Poll::Tunnel(Err(err)) => return Err(anyhow::anyhow!("TunnelStream read error: {err}")),
-            Poll::Local(Ok(0)) => return Err(anyhow::anyhow!("LocalStream closed")),
-            Poll::Tunnel(Ok(0)) => return Err(anyhow::anyhow!("TunnelStream closed")),
-            Poll::Local(Ok(n)) => {
-                log::debug!("Forwarding {} bytes from LocalStream to TunnelStream", n);
-                tunnel_stream.write_all(&local_buffer[..n]).await.map_err(|err| anyhow::anyhow!("TunnelStream write error: {err}"))?
-            }
-            Poll::Tunnel(Ok(n)) => {
-                log::debug!("Forwarding {} bytes from TunnelStream to LocalStream", n);
-                local_stream.write_all(&tunnel_buffer[..n]).await.map_err(|err| anyhow::anyhow!("LocalStream write error: {err}"))?
-            }
-        }
+        Err(err) => Err(anyhow::anyhow!("Stream error: {err}"))
     }
 }
